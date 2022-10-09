b58126430aa345d65be1c86ebde4c00f20c3978b
aacfahim <aacfahim@gmail.com>
2022-10-09 17:49:02 +0600

    api service

--- lib/constants/const.dart
+++ lib/constants/const.dart
@@ -1,2 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 var baseURL =
     "https://phonevalidation.abstractapi.com/v1/?api_key=4a0a78a73de54e3d9c7a4bb61f03eda6&phone=";
+
+showToast(String message) {
+  return Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.orange,
+      textColor: Colors.white,
+      fontSize: 16.0);
+}
--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,1 +1,34 @@
+import 'dart:convert';
 
+import 'package:phone_number_validator/constants/const.dart';
+import 'package:phone_number_validator/model/json_model.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  Future<List<JsonModel>> fetchData(String number) async {
+    List<JsonModel> numberInfoList = [];
+    JsonModel jsonModel;
+
+    var link = "$baseURL$number";
+    var response = await http.get(Uri.parse(link));
+    var data = jsonDecode(response.body);
+
+    print("data: $data");
+
+    try {
+      if (response.statusCode == 200) {
+        for (var i in data) {
+          jsonModel = JsonModel.fromJson(i);
+          numberInfoList.add(jsonModel);
+        }
+        return numberInfoList;
+      } else {
+        showToast("Something went wrong");
+        return numberInfoList;
+      }
+    } catch (e) {
+      print(e);
+      return numberInfoList;
+    }
+  }
+}
