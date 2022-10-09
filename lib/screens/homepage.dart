833e18d0c50880f608b8c3858092cfd94a81ff41
aacfahim <aacfahim@gmail.com>
2022-10-09 18:40:28 +0600

    homepage ui

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:phone_number_validator/model/json_model.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+  TextEditingController number = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text("Phone Number Validator")),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: number,
+              decoration: InputDecoration(
+                labelText: 'Enter a phone number',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0)),
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: () {}, child: Text("Search")),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return Placeholder();
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
