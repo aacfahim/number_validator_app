b730665c6a1cd51e8ef2a20706b9035d42af0519
aacfahim <aacfahim@gmail.com>
2022-10-09 17:40:33 +0600

    first commit

---
+++ lib/constants/const.dart
@@ -0,0 +1,2 @@
+var baseURL =
+    "https://phonevalidation.abstractapi.com/v1/?api_key=4a0a78a73de54e3d9c7a4bb61f03eda6&phone=";
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:phone_number_validator/screens/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of the application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Phone Number Validator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
